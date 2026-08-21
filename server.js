83edb5c0aca82c21a4aaa5c799b82e6d3007c152
Rohan Powar <rohanpawar2003rds@gmail.com>
2026-08-21 16:10:13 +0530

    Change from none to lax

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ const init = async () => {
       cookie: {
         name: 'oauth-session',
         password: process.env.COOKIE_PASSWORD,
-        isSecure: process.env.NODE_ENV === "production", // ⚠️ true in production (HTTPS)
-        isSameSite: "Lax", // ✅ Change from "Lax" to "None"
+        isSecure: true, // ⚠️ true in production (HTTPS)
+        isSameSite: "None", // ✅ Change from "Lax" to "None"
       },
       redirectTo: false,
     });

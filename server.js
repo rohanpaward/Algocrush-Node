2d8a5cb3c9c2699ced9ea75111880bad1ec49976
Rohan Powar <rohanpawar2003rds@gmail.com>
2026-08-21 17:54:33 +0530

    Revert code

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const init = async () => {
         name: 'oauth-session',
         password: process.env.COOKIE_PASSWORD,
         isSecure: true, // ⚠️ true in production (HTTPS)
-        isSameSite: "Lax", // ✅ Change from "Lax" to "None"
+        isSameSite: "None", // ✅ Change from "Lax" to "None"
       },
       redirectTo: false,
     });
--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -77,7 +77,7 @@ module.exports = [
   {
     method: httpProtocols.GET,
     path: authEndpoints.AUTH_ME,
-    options: { auth: false }, // IMPORTANT
+    options: { auth: 'jwt' }, // IMPORTANT
     handler: getMeController,
   }
 ];
